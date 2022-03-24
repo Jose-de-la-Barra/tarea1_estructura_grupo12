68d5ad62ed11db3a91cb3b0cd2b1eebcb35fd0ff
Jose-de-la-Barra <jose.delabarra2@gmail.com>
2022-03-24 00:22:49 -0300

    ahora se lee el .txt y se guarda en un arreglo

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
-// cambio //
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    FILE *fpointer = fopen("mensaje.txt", "r");
+
+    char text[150];
+    fgets(text, 150, fpointer);
+
+    printf("%s", text);
+
+    //printf("La cantidad de argumentos es: %d\n", argc);
+    //for (int i = 0; i < argc; ++i)
+    //    printf("El argumento %d es %s\n", i, argv[i]);
+}
+
