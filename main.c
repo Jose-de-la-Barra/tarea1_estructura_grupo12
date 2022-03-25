1b5d628d1ed681e0b7e50db94560238edf04b14b
Jose-de-la-Barra <jose.delabarra2@gmail.com>
2022-03-25 15:36:05 -0300

    agregar a main principal

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+
+void decode(char text[200], char word1[15], char word2[15]); /* recibe texto de entrada, dos palabras del mismo largo y
+                                                * archivo en donde se va a escribir la salida (por hacer)*/
+void encode(char text[200], char word1[15], char word2[15]);
+
 
 int main(int argc, char *argv[]) {
-
-    /* para recuperar el texto de entrada */
     FILE *fpointer;
     char text[200];
+    char word1[15];
+    char word2[15];
 
+    /* para recuperar el texto de entrada */
     fpointer = fopen(argv[1] , "r");
     if(fpointer == NULL) {
         perror("Error opening file");
@@ -19,28 +26,14 @@ int main(int argc, char *argv[]) {
     /* ya tenemos el texto de entrada */
 
 
-
     return 0;
 }
 
 
-//int main(int argc, char *argv[]) {
+void encode(char text[200], char word1[15], char word2[15]) {  // procedimiento para cifrar texto //
+}
 
-    //Cómo hacerlo, argv[3] y argv[4] son las palabras claves *revisar en verdad, puede que sean el [2] y [3]*, por lo tanto, lo que se puede hacer es, evaluar las palabras letra por letra
-    // y lograr reemplazar las letras de la primera palabra con los de la segunda, letra por letra(lista?????) para así hacer el cambio en el archivo, pero generando otro desde el argv[4]
-//    FILE *fin = fopen("mensaje.txt", "r");
-//    FILE *fout = fopen("mensaje_min.txt", "w");
-//    int c = fgetc(fin);
-//    while(c != EOF)
-//    {
-//        fputc(tolower(c), fout);
-//        c = fgetc(fin);
-//    }
+void decode(char text[200], char word1[15], char word2[15]) {
 
-//    fclose(fin);
-//    fclose(fout);
-
-//    return 0;
-    //USAR DE ESTA PARTE DEL CÓDIGO LO QUE HACE QUE LEA EL ARCHIVO MENSAJE.TXT Y GENERE OTRO ARCHIVO TEXT CON LAS ESPECIFICACIONES DADAS
-//}
+}
 
