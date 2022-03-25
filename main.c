fba7bba9062648ecfda90296eee7ebce47aa5124
pedrofajardop <67644308+pedrofajardop@users.noreply.github.com>
2022-03-25 14:31:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,11 @@ int main(int argc, char *argv[]) {
 
     /* para recuperar el texto de entrada */
     FILE *fpointer;
+
     char text[200];
+    char option[15];
+    char word1[15];
+    char word2[15];
 
     fpointer = fopen(argv[1] , "r");
     if(fpointer == NULL) {
@@ -17,17 +21,30 @@ int main(int argc, char *argv[]) {
     }
     fclose(fpointer);
     /* ya tenemos el texto de entrada */
+    option = argv[2];
+    if (option == "encode") {
+        printf("%s", option);
+    }
+    if (option == "decode") {
+        printf("%s", option);
+    }
+    else {
+        perror("Error proceso no definido");
+    }
 
+
+    /* ya tenemos el texto de entrada */
 
 
     return 0;
 }
 
 
+
 //int main(int argc, char *argv[]) {
 
-    //Cómo hacerlo, argv[3] y argv[4] son las palabras claves *revisar en verdad, puede que sean el [2] y [3]*, por lo tanto, lo que se puede hacer es, evaluar las palabras letra por letra
-    // y lograr reemplazar las letras de la primera palabra con los de la segunda, letra por letra(lista?????) para así hacer el cambio en el archivo, pero generando otro desde el argv[4]
+//Cómo hacerlo, argv[3] y argv[4] son las palabras claves *revisar en verdad, puede que sean el [2] y [3]*, por lo tanto, lo que se puede hacer es, evaluar las palabras letra por letra
+// y lograr reemplazar las letras de la primera palabra con los de la segunda, letra por letra(lista?????) para así hacer el cambio en el archivo, pero generando otro desde el argv[4]
 //    FILE *fin = fopen("mensaje.txt", "r");
 //    FILE *fout = fopen("mensaje_min.txt", "w");
 //    int c = fgetc(fin);
@@ -41,5 +58,5 @@ int main(int argc, char *argv[]) {
 //    fclose(fout);
 
 //    return 0;
-    //USAR DE ESTA PARTE DEL CÓDIGO LO QUE HACE QUE LEA EL ARCHIVO MENSAJE.TXT Y GENERE OTRO ARCHIVO TEXT CON LAS ESPECIFICACIONES DADAS
+//USAR DE ESTA PARTE DEL CÓDIGO LO QUE HACE QUE LEA EL ARCHIVO MENSAJE.TXT Y GENERE OTRO ARCHIVO TEXT CON LAS ESPECIFICACIONES DADAS
 //}
