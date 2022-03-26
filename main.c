d49a9f0c09c2d1f03feca7aef883f3848bcd867e
Jose-de-la-Barra <jose.delabarra2@gmail.com>
2022-03-26 17:38:32 -0300

    actualizado con el main

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdbool.h>
 
-void decode(char text[200], char word1[15], char word2[15]); /* recibe texto de entrada, dos palabras del mismo largo y
+void encode_decode(char text[200], char word1[15], char word2[15]); /* recibe texto de entrada, dos palabras del mismo largo y
                                                 * archivo en donde se va a escribir la salida (por hacer)*/
-void encode(char text[200], char word1[15], char word2[15]);
 
 void concatenate(char *cadena, char c);
 
 
 int main(int argc, char *argv[]) {
-
     FILE *fpointer;
     char text[200];
     char option1[] = {"encode"};
@@ -18,10 +17,15 @@ int main(int argc, char *argv[]) {
     char *word1[15] = {argv[3]};
     char *word2[15] = {argv[4]};
 
+    if (argc < 5 || argc > 5) {
+        printf("error: Debes ingresar 5 argumentos\n");
+        return 1;
+    }
+
     fpointer = fopen(argv[1], "r");
     if (fpointer == NULL) {
         perror("Opening file");
-        return (-1);
+        return -1;
     } else {
         fgets(text, 200, fpointer);
     }
@@ -30,35 +34,48 @@ int main(int argc, char *argv[]) {
     /* ya tenemos el texto de entrada */
 
     if (strcmp(argv[2],option1) == 0) {
-        encode(text, *word1, *word2);
+        encode_decode(text, *word1, *word2);
     }
     else if (strcmp(argv[2],option2) == 0) {
-        printf("decode\n");
+        encode_decode(text, *word1, *word2);
     }
     else {
         perror("Proceso no definido");
+        return -1;
     }
     return 0;
 }
 
 
-void encode(char text[200], char word1[15], char word2[15]) {  // procedimiento para cifrar texto //
+void encode_decode(char text[200], char word1[15], char word2[15]) {  // procedimiento para cifrar o descifrar texto //
 
     char *pointer; //puntero que apuntará a cada uno de los caracteres del texto principal
     char *p1;
     char *p2;
     char cadena[200] = "";
-
     int large_w1 = strlen(word1);
 
     pointer = text;  // primera letra de la cadena
+
 
     while (*pointer != '\0') {
         p1 = word1;
         p2 = word2;
         int cont = 0;
+        bool bool_text = false;
+
+        if(*pointer >= 'A' && *pointer <= 'Z') {
+            bool_text = true;
+        }
 
         while (*p1 != '\0') {
+
+            if (bool_text == true) {  /* si la letra del texto de entrada está en mayúscula, la letra que
+                                               * agregamos al texto de salida también tiene que estar en mayúscula */
+                *p1 = toupper(*p1);
+                *p2 = toupper(*p2);
+            }
+
             // *p1 recorre word1 letra por letra mientras que *pointer recorre letra por letra el texto
             if (*pointer != *p1 && *pointer != *p2) {
                 cont++;
@@ -66,29 +83,26 @@ void encode(char text[200], char word1[15], char word2[15]) {  // procedimiento 
             else if (*pointer == *p1 && *pointer != *p2 || *pointer != *p1 && *pointer == *p2) {
 
                 if (*pointer == *p1 && *pointer != *p2) {
+
                     concatenate(cadena, *p2);
                 }
                 else if (*pointer != *p1 && *pointer == *p2) {
                     concatenate(cadena, *p1);
                 }
             }
+            *p1 = tolower(*p1);
+            *p2 = tolower(*p2);
+
             p1++;
             p2++;
 
             if (cont == large_w1) {
                 concatenate(cadena, *pointer);
             }
-
         }
-
-
         pointer++;
     }
     printf("%s", cadena);
-}
-
-void decode(char text[200], char word1[15], char word2[15]) {
-
 }
 
 
@@ -118,3 +132,4 @@ void concatenate(char *cadena, char c)
 //    return 0;
 //USAR DE ESTA PARTE DEL CÓDIGO LO QUE HACE QUE LEA EL ARCHIVO MENSAJE.TXT Y GENERE OTRO ARCHIVO TEXT CON LAS ESPECIFICACIONES DADAS
 //}
+
