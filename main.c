d4ac43dea7e5321788ed9ebe667ab2d701de4144
Jose-de-la-Barra <jose.delabarra2@gmail.com>
2022-03-26 23:19:35 -0300

    Programa listogit add . Falta ver lo de CMakefile

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 
-void encode_decode(char text[200], char word1[15], char word2[15]); /* recibe texto de entrada, dos palabras del mismo largo y
+void encode_decode(char text[200], char word1[15], char word2[15], char arch2[15]); /* recibe texto de entrada, dos palabras del mismo largo y
                                                 * archivo en donde se va a escribir la salida (por hacer)*/
 
 void concatenate(char *cadena, char c);
@@ -17,17 +17,42 @@ int main(int argc, char *argv[]) {
     char *word1[15] = {argv[3]};
     char *word2[15] = {argv[4]};
 
-    // verificaciones antes de comenzar a llamar a los procedimientos //
+    char *arch1[15] = {argv[1]};
+    char *arch2[15] = {argv[5]};
 
-    if (strlen(*word1) != strlen(*word2)) {
-        perror("error: las palabras claves deben ser del mismo largo\n");
-        return 1;
+    char *p1; // puntero para recorrer *word1
+    char *p2;
+
+    /* verificaciones que todos los argumentos se hayan pasado
+     * bien antes de comenzar a llamar a los procedimientos */
+
+    if (argc != 6) {
+        printf("error: Debes ingresar 5 argumentos\n");
+        return -1;
     }
 
 
-    if (argc < 5 || argc > 5) {
-        printf("error: Debes ingresar 5 argumentos\n");
-        return 1;
+    if (strcmp(*arch1, *arch2) == 0) {
+        perror("Nombre del archivo de salida tiene que ser distinto al de entrada");
+        return -1;
+    }
+
+    p1 = *word1;
+    while (*p1 != '\0') {
+        p2 = *word2;
+        while (*p2 != '\0') {
+            if (*p1 == *p2) {
+                perror("error: las palabras clave no pueden contener las mismas letras");
+                return -1;
+            }
+            p2++;
+        }
+        p1++;
+    }
+
+    if (strlen(*word1) != strlen(*word2)) {
+        perror("error: las palabras clave deben ser del mismo largo\n");
+        return -1;
     }
 
     fpointer = fopen(argv[1], "r");
@@ -35,27 +60,26 @@ int main(int argc, char *argv[]) {
         perror("Opening file");
         return -1;
     } else {
-        fgets(text, 200, fpointer);
+        fgets(text, 200, fpointer); /* ya tenemos el texto de entrada */
     }
     fclose(fpointer);
 
-    /* ya tenemos el texto de entrada */
-
+    // inicializamos los procedimientos para codificar o decodificar //
     if (strcmp(argv[2],option1) == 0) {
-        encode_decode(text, *word1, *word2);
+        encode_decode(text, *word1, *word2, *arch2);
     }
     else if (strcmp(argv[2],option2) == 0) {
-        encode_decode(text, *word1, *word2);
+        encode_decode(text, *word1, *word2, *arch2);
     }
     else {
-        perror("Proceso no definido");
+        perror("El segundo argumento debe ser decode o encode");
         return -1;
     }
     return 0;
 }
 
 
-void encode_decode(char text[200], char word1[15], char word2[15]) {  // procedimiento para cifrar o descifrar texto //
+void encode_decode(char text[200], char word1[15], char word2[15], char arch2[15]) {  // procedimiento para cifrar o descifrar texto //
 
     char *pointer; //puntero que apuntará a cada uno de los caracteres del texto principal
     char *p1;
@@ -70,7 +94,8 @@ void encode_decode(char text[200], char word1[15], char word2[15]) {  // procedi
         p1 = word1;
         p2 = word2;
         int cont = 0;
-        bool bool_text = false;
+        bool bool_text = false;  /* creamos booleanos para tener un indicador para cuando
+                                  * la letra que revisamos es mayúscula */
 
         if(*pointer >= 'A' && *pointer <= 'Z') {
             bool_text = true;
@@ -107,7 +132,6 @@ void encode_decode(char text[200], char word1[15], char word2[15]) {  // procedi
                             concatenate(cadena, *p2);
                         }
                     }
-
                 }
                 else if (tolower(*pointer) != tolower(*p1) && tolower(*pointer) == tolower(*p2)) {
                     if (bool_text == true) {
@@ -121,11 +145,8 @@ void encode_decode(char text[200], char word1[15], char word2[15]) {  // procedi
                             concatenate(cadena, *p1);
                         }
                     }
-
                 }
             }
-
-
             p1++;
             p2++;
 
@@ -135,9 +156,12 @@ void encode_decode(char text[200], char word1[15], char word2[15]) {  // procedi
         }
         pointer++;
     }
-    printf("%s", cadena);
-}
 
+    const char* filename = arch2;
+    FILE* output_file = fopen(filename, "w+");
+    fwrite(cadena, 1, strlen(cadena), output_file);
+    fclose(output_file);
+}
 
 void concatenate(char *cadena, char c)
 {
@@ -146,23 +170,3 @@ void concatenate(char *cadena, char c)
     cadenaTemporal[1] = '\0';
     strcat(cadena, cadenaTemporal);
 }
-
-
-
-//int main(int argc, char *argv[]) {
-
-//Cómo hacerlo, argv[3] y argv[4] son las palabras claves *revisar en verdad, puede que sean el [2] y [3]*, por lo tanto, lo que se puede hacer es, evaluar las palabras letra por letra
-// y lograr reemplazar las letras de la primera palabra con los de la segunda, letra por letra(lista?????) para así hacer el cambio en el archivo, pero generando otro desde el argv[4]
-//    FILE *fin = fopen("mensaje.txt", "r");
-//    FILE *fout = fopen("mensaje_min.txt", "w");
-//    int c = fgetc(fin);
-//    while(c != EOF)
-//    {
-//        fputc(tolower(c), fout);
-//        c = fgetc(fin);
-//    }
-
-//    return 0;
-//USAR DE ESTA PARTE DEL CÓDIGO LO QUE HACE QUE LEA EL ARCHIVO MENSAJE.TXT Y GENERE OTRO ARCHIVO TEXT CON LAS ESPECIFICACIONES DADAS
-//}
-
